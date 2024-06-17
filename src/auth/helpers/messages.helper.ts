df4aaffe46bda10565dc3e7387ce9a4611720bf7
Guilherme Mariano de Oliveira <guilherme_mariano95@hotmail.com>
2024-06-17 20:36:09 -0300

    login

---
+++ src/auth/helpers/messages.helper.ts
@@ -0,0 +1,5 @@
+export const MessagesHelper = {
+    AUTH_LOGIN_NOT_FOUND: 'login informado nao é valido',
+    AUTH_PASSWORD_NOT_FOUND: 'senha informada nao é valida',
+    AUTH_PASSWORD_OR_LOGIN_NOT_FOUND: 'e-mail e/ou senha nao econtrados.',
+}
