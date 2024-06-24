2dd76ae59398a7cd0df340f8d0fb34d943800182
Guilherme Mariano de Oliveira <guilherme_mariano95@hotmail.com>
2024-06-24 20:44:43 -0300

    Aplicando Guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Controller, HttpCode, HttpStatus, Post, Body } from '@nestjs/common'
 import { AuthService } from './auth.service';
 import { LoginDto } from './dtos/login.dto';
 import { RegisterDto } from 'src/user/dtos/register.dto';
+import { IsPublic } from './decorators/ispublic.decorator';
 
 @Controller("auth")
 export class AuthController{
@@ -9,6 +10,7 @@ export class AuthController{
 
     @Post('login')
     @HttpCode(HttpStatus.OK)
+    @IsPublic()
     login(@Body() dto: LoginDto){
         return this.authService.login(dto);
 
@@ -16,6 +18,7 @@ export class AuthController{
 
     @Post('register')
     @HttpCode(HttpStatus.OK)
+    @IsPublic()
     register(@Body() dto: RegisterDto){
         return this.authService.register(dto);
 
