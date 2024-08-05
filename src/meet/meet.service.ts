db0e23acf610c86fe4386a28db0cdda23e523449
Guilherme Mariano de Oliveira <guilherme_mariano95@hotmail.com>
2024-08-05 12:01:13 -0300

    GET POST DELETE

--- src/meet/meet.service.ts
+++ src/meet/meet.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Meet, MeetDocument } from './schemas/meet.schema';
+import { UserService } from 'src/user/user.service';
+import { Model } from 'mongoose';
+import { CreateMeetDto } from './dtos/createmeet.dto';
+import { generateLink } from './helpers/linkgerenator.helper';
+
 
 @Injectable()
-export class MeetService {}
+export class MeetService {
+    private readonly logger = new Logger(MeetService.name);
+
+    constructor(
+        @InjectModel(Meet.name) private readonly model: Model<MeetDocument>,
+        private readonly userService: UserService
+    ){}
+
+    async getMeetsByUser(userId:String){
+        this.logger.debug('getMeetsByUser - ' + userId);
+        return await this.model.find({user: userId});
+    }
+
+    async createMeet(userId:string, dto:CreateMeetDto){
+        this.logger.debug('createMeet - ' + userId);
+
+        const user = await this.userService.getUserById(userId);
+
+        const meet = {
+            ...dto,
+            user,
+            link: generateLink()
+        };
+
+        const createdMeet = new this.model(meet);
+        return await createdMeet.save();
+    }
+
+    async deleteMeetByUser(userId:String, meetId:string){
+        this.logger.debug(`deleteMeetByUser - ${userId} - ${meetId}`);
+        return await this.model.deleteOne({user: userId, _id: meetId});
+    }
+}
